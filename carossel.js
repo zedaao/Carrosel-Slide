b721405620a66e7f1a3497e6b1d852163a89848d
leon <leon.ueda2016@gmail.com>
2022-08-09 14:04:09 -0300

    first commit

---
+++ carossel.js
@@ -0,0 +1,21 @@
+let time = 5000,
+  currentImageIndex = 0,
+  images = document.querySelectorAll('#slider img');
+max = images.length;
+
+function nextImage() {
+  images.forEach((img) => img.classList.remove('selected'));
+
+  currentImageIndex++;
+  if (currentImageIndex >= max) currentImageIndex = 0;
+
+  images[currentImageIndex].classList.add('selected');
+}
+
+function start() {
+  setInterval(() => {
+    nextImage();
+  }, time);
+}
+
+window.addEventListener('load', start);
